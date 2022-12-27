141390c4702288d403fed95069109fef66a20bb8
Kaique da Silva <kaeyosthaeron@gmail.com>
2022-12-26 21:00:03 -0300

    added pkg to use on gh pages

---
+++ pkg/README.md
@@ -0,0 +1,43 @@
+# Brainfuck Interpreter
+
+## Description
+
+Yet another [brainfuck](https://en.wikipedia.org/wiki/Brainfuck) interpreter as part of a
+train, early on my career I was fascinated by the fact of create other pieces of technology as
+programming language with my knowledge and learn with my failures is the best parte in my
+humble option.
+
+So I have created this project to put in practice and I need your help to write valid program
+to put the interpreter on fire proof.
+
+## Usage
+
+Clone the project on your machine:
+
+```
+git clone https://github.com/ktfth/brainfuck-rs
+```
+
+After that you can continue with the installation of rust(optional if present):
+
+```
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+```
+
+Or just visiting the `https://rustup.rs` to see other instructions. With this in mind
+you can follow the next steps as run to see the project in action.
+
+```
+cargo run -- ./examples/input.bf
+```
+
+Note: The file can be whatever you need.
+
+You eve gonna see the output with a simple `Hello World!` and yay, you made it.
+
+# Contribute
+
+This project are based on 3-Clause BSD License, so you can freely contribute or extend as
+you want, if you write a program who is not valid, open an issue or just create a test
+and pass it. Suggestions are welcome: kaeyosthaeron@gmail.com so email me to get things up and
+running.
---
+++ pkg/brainfuck_bg.js
@@ -0,0 +1,76 @@
+import * as wasm from './brainfuck_bg.wasm';
+
+let WASM_VECTOR_LEN = 0;
+
+let cachedUint8Memory0 = new Uint8Array();
+
+function getUint8Memory0() {
+    if (cachedUint8Memory0.byteLength === 0) {
+        cachedUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachedUint8Memory0;
+}
+
+const lTextEncoder = typeof TextEncoder === 'undefined' ? (0, module.require)('util').TextEncoder : TextEncoder;
+
+let cachedTextEncoder = new lTextEncoder('utf-8');
+
+const encodeString = (typeof cachedTextEncoder.encodeInto === 'function'
+    ? function (arg, view) {
+    return cachedTextEncoder.encodeInto(arg, view);
+}
+    : function (arg, view) {
+    const buf = cachedTextEncoder.encode(arg);
+    view.set(buf);
+    return {
+        read: arg.length,
+        written: buf.length
+    };
+});
+
+function passStringToWasm0(arg, malloc, realloc) {
+
+    if (realloc === undefined) {
+        const buf = cachedTextEncoder.encode(arg);
+        const ptr = malloc(buf.length);
+        getUint8Memory0().subarray(ptr, ptr + buf.length).set(buf);
+        WASM_VECTOR_LEN = buf.length;
+        return ptr;
+    }
+
+    let len = arg.length;
+    let ptr = malloc(len);
+
+    const mem = getUint8Memory0();
+
+    let offset = 0;
+
+    for (; offset < len; offset++) {
+        const code = arg.charCodeAt(offset);
+        if (code > 0x7F) break;
+        mem[ptr + offset] = code;
+    }
+
+    if (offset !== len) {
+        if (offset !== 0) {
+            arg = arg.slice(offset);
+        }
+        ptr = realloc(ptr, len, len = offset + arg.length * 3);
+        const view = getUint8Memory0().subarray(ptr + offset, ptr + len);
+        const ret = encodeString(arg, view);
+
+        offset += ret.written;
+    }
+
+    WASM_VECTOR_LEN = offset;
+    return ptr;
+}
+/**
+* @param {string} code
+*/
+export function run(code) {
+    const ptr0 = passStringToWasm0(code, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+    const len0 = WASM_VECTOR_LEN;
+    wasm.run(ptr0, len0);
+}
+
